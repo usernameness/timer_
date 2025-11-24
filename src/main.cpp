f39fc81671ab865afbdd1464760a84c9ca5c0fc6
cpar174 <87593691+cpar174@users.noreply.github.com>
2025-11-24 20:49:00 +1300

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,86 @@
 #include <Arduino.h>
+#include <wire.h>
+#include <RTClib.h>
+#include <Adafruit_SSD1306.h>
 
-// put function declarations here:
-int myFunction(int, int);
+
+// --- Pin definitions ---
+#define SDA_PIN 6
+#define SCL_PIN 5
+#define ENC_A   9
+#define ENC_B   10
+#define ENC_SW  4
+#define LED_PIN 8
+
+// OLED Display Defines
+#define SCREEN_WIDTH 128 
+#define SCREEN_HEIGHT 64
+#define OLED_ADDR   0x3C
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+RTC_DS3231 rtc;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+  Serial.begin(9600);
+
+  pinMode(LED_PIN, OUTPUT);
+  Wire.begin(SDA_PIN, SCL_PIN);
+
+  if(!rtc.begin()) {
+    Serial.println(F("Couldn't find RTC"));
+    for(;;); // Halt if deiplay not found
+  }
+
+  if(rtc.lostPower()) {
+    Serial.println(F("RTC lost power, setting the time!"));
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Halt if deiplay not found
+  }
+
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0,0);
+  display.println("Hello, World!"); 
+  display.display();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  digitalWrite(LED_PIN, HIGH); 
+  delay(500);                   
+  digitalWrite(LED_PIN, LOW);    
+  delay(500);
+  
+  DateTime now = rtc.now();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // Clear screen before drawing
+  display.clearDisplay();
+
+  // Print time
+  display.setCursor(0, 0);
+  display.setTextSize(2);
+  display.print(now.hour());
+  display.print(":");
+  if (now.minute() < 10) display.print("0"); // leading zero
+  display.print(now.minute());
+  display.print(":");
+  if (now.second() < 10) display.print("0");
+  display.print(now.second());
+
+  // Print date
+  display.setTextSize(1);
+  display.setCursor(0, 30);
+  display.print(now.day());
+  display.print("/");
+  display.print(now.month());
+  display.print("/");
+  display.print(now.year());
+
+  display.display();
 }
